a314bd6526f7ec5bd901ab5a26843dc3cb89406b
Nakahama <k-nakahama@esol.co.jp>
2026-02-13 14:43:10 +0900

    merge from bare repos 2026/02/13

--- AbstractModule/InputAdapter.py
+++ AbstractModule/InputAdapter.py
@@ -1,3 +1,5 @@
+import pygame
+from pygame.locals import *
 from SimpleTetris.eventdef import GameEvent
 
 # ============================================================
@@ -16,25 +18,54 @@ class ConsoleInputAdapter:
     何も入力せず Enter なら TICK として扱う。
     """
 
-    def get_event(self) -> GameEvent:
+    def get_event(self) -> list[GameEvent]:
         s = input("Command (a/d/w/s/space/q, Enter=tick): ").strip().lower()
+        ret = []
         if s == "a":
-            return GameEvent.INPUTEVENT_LEFT
+            ret.append(GameEvent.INPUTEVENT_LEFT)
         if s == "d":
-            return GameEvent.INPUTEVENT_RIGHT
+            ret.append(GameEvent.INPUTEVENT_RIGHT)
         if s == "w":
-            return GameEvent.INPUTEVENT_ROTATE
+            ret.append(GameEvent.INPUTEVENT_ROTATE) 
         if s == "s":
-            return GameEvent.INPUTEVENT_SOFT_DROP
+            ret.append(GameEvent.INPUTEVENT_SOFT_DROP)
         if s == " " or s == "space":
-            return GameEvent.INPUTEVENT_HARD_DROP
+            ret.append(GameEvent.INPUTEVENT_HARD_DROP)
         if s == "q":
-            return GameEvent.INPUTEVENT_QUIT
-        return GameEvent.INPUTEVENT_TICK
+            ret.append(GameEvent.INPUTEVENT_QUIT)
+        if not ret:
+            ret.append(GameEvent.INPUTEVENT_TICK)
+        return ret
 
-
+class PygameInputAdapter:
+    def get_event(self) -> list[GameEvent]:
+        ret = []
+        # イベント処理
+        for event in pygame.event.get():
+            # 閉じるボタンが押されたら終了
+            if event.type == QUIT: 
+                ret.append( GameEvent.INPUTEVENT_QUIT )
+            # キーイベント
+            if event.type == KEYDOWN:
+                # Escキーが押されたら終了
+                if event.key == K_ESCAPE:
+                    ret.append( GameEvent.INPUTEVENT_QUIT )
+                # 矢印キーなら円の中心座標を矢印の方向に移動
+                if event.key == K_LEFT:
+                    ret.append( GameEvent.INPUTEVENT_LEFT )
+                if event.key == K_RIGHT:
+                    ret.append( GameEvent.INPUTEVENT_RIGHT )
+                if event.key == K_UP:
+                    ret.append( GameEvent.INPUTEVENT_ROTATE )
+                if event.key == K_DOWN:
+                    ret.append( GameEvent.INPUTEVENT_SOFT_DROP )   # 使わないけど、一応対応しておく
+                if event.key == K_SPACE:
+                    ret.append( GameEvent.INPUTEVENT_HARD_DROP )
+        ret.append(GameEvent.INPUTEVENT_TICK)
+        return ret
+    
 class InputAdapter:
     def __init__(self):
-        self.core = ConsoleInputAdapter()
-    def get_event(self) -> GameEvent:
+        self.core = PygameInputAdapter()
+    def get_event(self) -> list[GameEvent]:
         return self.core.get_event()
--- tetris.py
+++ tetris.py
@@ -6,6 +6,8 @@ import random
 import os
 import sys
 import time
+import pygame
+from pygame.locals import *
 
 # Allow running both as `python -m tetris.tetris` and `python tetris/tetris.py`
 if __package__ is None or __package__ == "":
@@ -29,6 +31,12 @@ from SimpleTetris.AbstractModule.common_tool.EventBus import EventBus
 # メインループ
 # ============================================================
 def run():
+    # pygameの初期化
+    pygame.init()                                   # Pygameの初期化
+    screen = pygame.display.set_mode((1, 1), pygame.NOFRAME)
+    pygame.display.set_caption("SimpleTetris")              # 画面上部に表示するタイトルを設定
+    
+    # ゲームロジック用のオブジェクト群を生成
     model = GameModel.initial()
     updater = GameUpdater()
     view = GameView()
@@ -37,7 +45,9 @@ def run():
 
     while True:
         if run_gameloop_once(model, updater, view, input_adapter, eventbus):
-            break        
+            break
+
+    pygame.quit()
     print("Bye!")
 
 # 外部ゲームループから呼ばれるメイン処理
@@ -46,11 +56,12 @@ def run_gameloop_once(model, updater, view, input_adapter, eventbus):
     if model.is_GameOver():
         return True
 
-    eventbus.emit(input_adapter.get_event())
+    # 入力イベントの詰め替え
+    for event in input_adapter.get_event():
+        eventbus.emit(event)
     model = updater(model, eventbus)
     eventbus.end_tick()
     return False
-    
 
 if __name__ == "__main__":
     run()
