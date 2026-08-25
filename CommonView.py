790d09261c858bde0e8c3b945f8f7ade6cccb005
Nakahama <k-nakahama@esol.co.jp>
2026-08-25 16:25:00 +0900

    commit漏れ

---
+++ CommonView.py
@@ -0,0 +1,29 @@
+from SimpleTetris.view_base import ViewBase
+from SimpleTetris.GameModel import GameModel
+from SimpleTetris.AbstractModule.GraphicsAdapter import GraphicsAdapter, ConsoleGraphicsAdapter, ViewModel
+
+class CommonView(ViewBase):
+    def __init__(self, gfx: GraphicsAdapter = None):
+        self.gfx = gfx if gfx is not None else ConsoleGraphicsAdapter()
+    def __call__(self, state: GameModel):
+        # 固定ブロック＋落下中ピースを合成した一時ボードを作る
+        tmp = [row[:] for row in state.matrix.grid]
+        px, py = state.active_pos
+        for bx, by in state.active_piece.blocks():
+            x = px + bx
+            y = py + by
+            if 0 <= y < state.matrix.height and 0 <= x < state.matrix.width:
+                tmp[y][x] = 2  # 操作中ピースは 2 として区別
+
+        vm = ViewModel(
+            width=state.matrix.width,
+            height=state.matrix.height,
+            cells=tmp,
+            score=state.score,
+            lines=state.lines,
+            level=state.level,
+            goal=state.goal,
+            game_over=state.game_over,
+        )
+        # GraphicsAdapter のAPIコール
+        self.gfx.render(vm)
